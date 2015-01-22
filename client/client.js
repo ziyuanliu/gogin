2737d9aef58d0dad9b656fbfb8414ae63f7c35a0
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-01-22 14:03:07 -0500

    added pushstream

--- client/client.js
+++ client/client.js
@@ -1,21 +1,48 @@
-var ws = new WebSocket("ws://104.236.97.245/subscribe");
-// var ws1 = new WebSocket("ws://127.0.0.1:9080/subscribe");
-ws.onclose = function() { // thing to do on close
-    $('#con_status').val("closed");
-};
+// var ws = new WebSocket("ws://104.236.97.245/subscribe");
+// // var ws1 = new WebSocket("ws://127.0.0.1:9080/subscribe");
+// ws.onclose = function() { // thing to do on close
+//     $('#con_status').val("closed");
+// };
+//
+// ws.onerror = function(error) { // thing to do on error
+//     $('#con_status').val("Error "+error);
+// };
+//
+// ws.onmessage = function(e) { // thing to do on message
+//     $("#received").prepend("<li>"+e.data+"</li>");
+//     var num = parseInt($("#msg_received").text());
+//     num+=1;
+//     $("#msg_received").text(num.toString());
+// };
+//
+// ws.onopen = function() { // thing to do on open
+//     ws.send("hi");
+//     $("#con_status").text("open");
+// };
 
-ws.onerror = function(error) { // thing to do on error
-    $('#con_status').val("Error "+error);
-};
 
-ws.onmessage = function(e) { // thing to do on message
+var pushstream = new PushStream({
+    host:104.236.97.245,
+    port:80,
+    modes: 'websocket'
+});
+
+pushstream.onmessage = function(e) {
+    console.log(e);
     $("#received").prepend("<li>"+e.data+"</li>");
     var num = parseInt($("#msg_received").text());
     num+=1;
     $("#msg_received").text(num.toString());
 };
-
-ws.onopen = function() { // thing to do on open
-    ws.send("hi");
-    $("#con_status").text("open");
-};
+pushstream.onestatuschange = function(status){
+    console.log("new status",status);
+    if (status==PushStream.OPEN){
+        $('#con_status').val("open");
+    }else if (status==PushStream.CLOSED){
+        $('#con_status').val("closed");
+    }
+}
+pushstream.onerror = function(error){
+    console.log("error",error);
+}
+pushstream.connect();
